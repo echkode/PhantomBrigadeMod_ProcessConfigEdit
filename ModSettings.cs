d46e5c2f32e5feb5a233fa1673f5d23f5d39f0cd
EchKode <48565771+echkode@users.noreply.github.com>
2023-03-11 07:00:57 +0000

    Add files

---
+++ ModSettings.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+using UnityEngine;
+
+namespace EchKode.PBMods.ProcessConfigEdit
+{
+	partial class ModLink
+	{
+		internal sealed class ModSettings
+		{
+#pragma warning disable CS0649
+			public bool logging;
+#pragma warning restore CS0649
+		}
+
+		internal static ModSettings Settings;
+
+		static void LoadSettings()
+		{
+			var settingsPath = Path.Combine(modPath, "settings.yaml");
+			Settings = UtilitiesYAML.ReadFromFile<ModSettings>(settingsPath, false);
+			if (Settings == null)
+			{
+				Settings = new ModSettings();
+
+				Debug.LogFormat(
+					"Mod {0} ({1}) no settings file found, using defaults | path: {2}",
+					modIndex,
+					modID,
+					settingsPath);
+			}
+
+			if (Settings.logging)
+			{
+				Debug.LogFormat(
+					"Mod {0} ({1}) diagnostic logging is on: {2}",
+					modIndex,
+					modID,
+					Settings.logging);
+			}
+		}
+	}
+}
---
+++ Patch.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+
+using PBModManager = PhantomBrigade.Mods.ModManager;
+
+using UnityEngine;
+
+namespace EchKode.PBMods.ProcessConfigEdit
+{
+	[HarmonyPatch]
+	static class Patch
+	{
+		[HarmonyPatch(typeof(PBModManager), "ProcessFieldEdit")]
+		[HarmonyPrefix]
+		static bool Mm_ProcessFieldEditPrefix(
+			object target,
+			string filename,
+			string fieldPath,
+			string valueRaw,
+			int i,
+			string modID,
+			string dataTypeName)
+		{
+			var spec = new ModManager.EditSpec()
+			{
+				i = i,
+				modID = modID,
+				filename = ModManager.FindConfigKeyIfEmpty(target, dataTypeName, filename),
+				dataTypeName = dataTypeName,
+				root = target,
+				fieldPath = fieldPath,
+				valueRaw = valueRaw,
+			};
+			if (ModLink.Settings.logging)
+			{
+				Debug.LogFormat(
+					"Mod {0} ({1}) applying edit to config {2} path {3}",
+					spec.i,
+					spec.modID,
+					spec.filename,
+					spec.fieldPath);
+			}
+			ModManager.ProcessFieldEdit(spec);
+			return false;
+		}
+	}
+}
